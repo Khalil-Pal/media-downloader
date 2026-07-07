5a49f254f5515a835fccdc38babeeaf6bd0d1e5d
Khalil-Pal <h.hoshieh@yandex.ru>
2026-07-07 14:16:45 +0300

    Remove schedule module and related exports

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,7 +1,6 @@
 from .validators import is_valid_url, detect_platform, extract_url_from_text
 from .formatters import format_duration, format_size, truncate, progress_bar, escape_md
 from .rate_limiter import rate_limiter
-from .schedule import is_open, get_status_message
 
 __all__ = [
     "is_valid_url",
@@ -13,6 +12,4 @@ __all__ = [
     "progress_bar",
     "escape_md",
     "rate_limiter",
-    "is_open",
-    "get_status_message",
 ]
